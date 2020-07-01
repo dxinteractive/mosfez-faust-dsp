87b0659d2b55fddee6c4fa3022a0b3543183ba0a
Damien Clarke <dxinteractive@gmail.com>
2020-07-01 22:53:45 +1000

    notes and offsets with soundfile

--- test-nodejs/soundtest.js
+++ test-nodejs/soundtest.js
@@ -3,6 +3,20 @@ let faust = require('./soundtest.node');
 var dspFaustNode = new faust.DspFaustNode(44100,512);
 dspFaustNode.start();
 
+
+
+let on = false;
+let toggle = () => {
+    on = !on;
+    dspFaustNode.setParamValue("freq", Math.random() * 440 + 440);
+    dspFaustNode.setParamValue("gate", on ? 1 : 0);
+    setTimeout(() => {
+        toggle();
+    }, 120);
+};
+
+toggle();
+
 setTimeout(() => {
     console.log('stop');
 
