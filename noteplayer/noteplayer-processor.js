9093927c976e38e6a7600258a66f8357eb10f2cf
Damien Clarke <dxinteractive@gmail.com>
2020-07-30 18:52:53 +1000

    feat: notsasdsefsdfsdf

---
+++ noteplayer/noteplayer-processor.js
@@ -0,0 +1,361 @@
+
+/*
+Code generated with Faust version 2.26.1
+Compilation options: -lang wasm-ib -scal -ftz 2
+*/
+
+function getJSONnoteplayer() {
+	return '{"name": "noteplayer","filename": "noteplayer.dsp","version": "2.26.1","compile_options": "-lang wasm-ib -scal -ftz 2","library_list": ["/usr/local/share/faust/stdfaust.lib","/usr/local/share/faust/oscillators.lib","/usr/local/share/faust/maths.lib","/usr/local/share/faust/platform.lib","/usr/local/share/faust/basics.lib"],"include_pathnames": ["/usr/local/share/faust","/usr/local/share/faust","/usr/share/faust",".","/Users/damienclarke/projects/mosfez-faust/noteplayer"],"size": 262464,"inputs": 0,"outputs": 2,"meta": [ { "basics.lib/name": "Faust Basic Element Library" },{ "basics.lib/version": "0.1" },{ "filename": "noteplayer.dsp" },{ "maths.lib/author": "GRAME" },{ "maths.lib/copyright": "GRAME" },{ "maths.lib/license": "LGPL with exception" },{ "maths.lib/name": "Faust Math Library" },{ "maths.lib/version": "2.3" },{ "name": "noteplayer" },{ "oscillators.lib/name": "Faust Oscillator Library" },{ "oscillators.lib/version": "0.1" },{ "platform.lib/name": "Generic Platform Library" },{ "platform.lib/version": "0.1" }],"ui": [ {"type": "vgroup","label": "noteplayer","items": []}]}';
+}
+function getBase64Codenoteplayer() { return "AGFzbQEAAAAB1oCAgAAQYAJ/fwBgBH9/f38AYAF/AX9gAX8Bf2ACf38BfWABfwF/YAJ/fwBgAX8AYAJ/fwBgAn9/AGABfwBgAn9/AX9gAn9/AX9gAn19AX1gA39/fQBgAX0BfQKZgICAAAIDZW52BV9wb3dmAA0DZW52BV9zaW5mAA8Dj4CAgAAOAAECAwQFBgcICQoLDA4FjICAgAABAYiAgIAA8IeAgAAHuoGAgAAMB2NvbXB1dGUAAwxnZXROdW1JbnB1dHMABA1nZXROdW1PdXRwdXRzAAUNZ2V0UGFyYW1WYWx1ZQAGDWdldFNhbXBsZVJhdGUABwRpbml0AAgNaW5zdGFuY2VDbGVhcgAJEWluc3RhbmNlQ29uc3RhbnRzAAoMaW5zdGFuY2VJbml0AAsaaW5zdGFuY2VSZXNldFVzZXJJbnRlcmZhY2UADA1zZXRQYXJhbVZhbHVlAA8GbWVtb3J5AgAK8JOAgAAOiIWAgAABBX9BACECQQAhBEEAIQVBACEGQQBBxQA2AuCBEEEAQX82AuSBEEEAQckANgLogRBBAEF/NgLsgRBBAEHHADYC8IEQQQBByQA2AvSBEEEAQccANgL4gRBBAEEANgL8gRBBAEEANgLMgRBBACECA0ACQCACQQJ0QeCBEEEAKALMgRBBAnRqKAIANgIAQQBBAUEAKALMgRBqQQhvNgLMgRAgAkEBaiECIAJBCEgEQAwCDAELCwtBACEDQQAhAwNAAkBB0IEQIANBAnRqQQA2AgAgA0EBaiEDIANBAkgEQAwCDAELCwtBACEEA0ACQEEAQQAoAtSBEEEBajYC0IEQQSAgBEECdGpD2w/JOEEAKALQgRBBf2qylBABOAIAQQBBACgC0IEQNgLUgRAgBEEBaiEEIARBgIAESARADAIMAQsLC0EAQckANgKAghBBAEF/NgKEghBBAEHMADYCiIIQQQBBfzYCjIIQQQBBygA2ApCCEEEAQcwANgKUghBBAEHKADYCmIIQQQBBADYCnIIQQQBBADYC2IEQQQAhBQNAAkBBoIAQIAVBAnRqQYCCEEEAKALYgRBBAnRqKAIANgIAQQBBAUEAKALYgRBqQQhvNgLYgRAgBUEBaiEFIAVBCEgEQAwCDAELCwtBAEEtNgKgghBBAEF/NgKkghBBAEEANgKoghBBAEEANgKsghBBAEEoNgKwghBBAEF/NgK0ghBBAEEANgK4ghBBAEEANgK8ghBBAEEANgLcgRBBACEGA0ACQEHAgBAgBkECdGpBoIIQQQAoAtyBEEECdGooAgA2AgBBAEEBQQAoAtyBEGpBCG82AtyBECAGQQFqIQYgBkEISARADAIMAQsLCwuxiICAAAIKfwp9QQAhBEEAIQVBACEGQwAAAAAhDkEAIQdBACEIQQAhCUMAAAAAIQ9DAAAAACEQQwAAAAAhEUMAAAAAIRJBACEKQQAhC0MAAAAAIRNDAAAAACEUQQAhDEEAIQ1DAAAAACEVQwAAAAAhFkMAAAAAIRcgA0EAaigCACEEIANBBGooAgAhBUEAIQYDQAJAQQAqAuiAEEEAKgLwgBBBACoC6IAQQQAqAvCAEJKOk5IhDkEAIA68QYCAgPwHcQR9IA4FQwAAAAALOALsgBBDAAAAQUEAKgLsgBCUqCEHIAdBAnQoAgAhCCAIQQBKIQlBACAIQQBHBH8gCQVBACgC+IAQCzYC9IAQQQAgCQR/IAgFQQAoAoSBEAs2AoCBEEEAKgKIgRBBACoCkIEQQQAqAoiBEEEAKgKQgRCSjpOSIQ9BACAPvEGAgID8B3EEfSAPBUMAAAAACzgCjIEQQ83MTD5BIEMAAIBHQQAqAoyBEJSoQQJ0aioCAJQhEEEAKgKYgRBBACoC/IAQQwAAAEBDq6qqPUEAKAKAgRCyIBCSQwAAisKSlBAAlJIhESARIBGOkyESQQAgErxBgICA/AdxBH0gEgVDAAAAAAs4ApSBEEGggBAgB0ECdGooAgAhCiAKQQBKIQtBACAKQQBHBH8gCwVBACgCoIEQCzYCnIEQQQAgCwR/IAoFQQAoAqiBEAs2AqSBEEEAKgKwgRBBACoC/IAQQwAAAEBDq6qqPUEAKAKkgRCyIBCSQwAAisKSlBAAlJIhEyATIBOOkyEUQQAgFLxBgICA/AdxBH0gFAVDAAAAAAs4AqyBEEHAgBAgB0ECdGooAgAhDCAMQQBKIQ1BACAMQQBHBH8gDQVBACgCuIEQCzYCtIEQQQAgDQR/IAwFQQAoAsCBEAs2AryBEEEAKgLIgRBBACoC/IAQQwAAAEBDq6qqPUEAKAK8gRCyIBCSQwAAisKSlBAAlJIhFSAVIBWOkyEWQQAgFrxBgICA/AdxBH0gFgVDAAAAAAs4AsSBEEPNzMw9QQAoAvSAELJBIEMAAIBHQQAqApSBEJSoQQJ0aioCAJRBACgCnIEQskEgQwAAgEdBACoCrIEQlKhBAnRqKgIAlJJBACgCtIEQskEgQwAAgEdBACoCxIEQlKhBAnRqKgIAlJKUIRcgBCAGaiAXOAIAIAUgBmogFzgCAEEAQQAqAuyAEDgC8IAQQQBBACgC9IAQNgL4gBBBAEEAKAKAgRA2AoSBEEEAQQAqAoyBEDgCkIEQQQBBACoClIEQOAKYgRBBAEEAKAKcgRA2AqCBEEEAQQAoAqSBEDYCqIEQQQBBACoCrIEQOAKwgRBBAEEAKAK0gRA2AriBEEEAQQAoAryBEDYCwIEQQQBBACoCxIEQOALIgRAgBkEEaiEGIAZBBCABbEgEQAwCDAELCwsLhYCAgAAAQQAPC4WAgIAAAEECDwuLgICAAAAgACABaioCAA8LioCAgAAAQQAoAuCAEA8LjoCAgAAAIAAgARACIAAgARALC6OEgIAAAQt/QQAhAUEAIQJBACEDQQAhBEEAIQVBACEGQQAhB0EAIQhBACEJQQAhCkEAIQtBACEBA0ACQEHsgBAgAUECdGpDAAAAADgCACABQQFqIQEgAUECSARADAIMAQsLC0EAIQIDQAJAQfSAECACQQJ0akEANgIAIAJBAWohAiACQQJIBEAMAgwBCwsLQQAhAwNAAkBBgIEQIANBAnRqQQA2AgAgA0EBaiEDIANBAkgEQAwCDAELCwtBACEEA0ACQEGMgRAgBEECdGpDAAAAADgCACAEQQFqIQQgBEECSARADAIMAQsLC0EAIQUDQAJAQZSBECAFQQJ0akMAAAAAOAIAIAVBAWohBSAFQQJIBEAMAgwBCwsLQQAhBgNAAkBBnIEQIAZBAnRqQQA2AgAgBkEBaiEGIAZBAkgEQAwCDAELCwtBACEHA0ACQEGkgRAgB0ECdGpBADYCACAHQQFqIQcgB0ECSARADAIMAQsLC0EAIQgDQAJAQayBECAIQQJ0akMAAAAAOAIAIAhBAWohCCAIQQJIBEAMAgwBCwsLQQAhCQNAAkBBtIEQIAlBAnRqQQA2AgAgCUEBaiEJIAlBAkgEQAwCDAELCwtBACEKA0ACQEG8gRAgCkECdGpBADYCACAKQQFqIQogCkECSARADAIMAQsLC0EAIQsDQAJAQcSBECALQQJ0akMAAAAAOAIAIAtBAWohCyALQQJIBEAMAgwBCwsLC+KAgIAAAEEAIAE2AuCAEEEAQwCAO0hDAACAP0EAKALggBCyl5Y4AuSAEEEAQwAAgD9BACoC5IAQlTgC6IAQQQBDAADcQ0EAKgLkgBCVOAL8gBBBAEMAACBBQQAqAuSAEJU4AoiBEAuQgICAAAAgACABEAogABAMIAAQCQuCgICAAAALkICAgAAAIAAgAUgEfyABBSAACw8LkICAgAAAIAAgAUgEfyAABSABCw8LjICAgAAAIAAgAWogAjgCAAsL0YiAgAABAEEAC8oIeyJuYW1lIjogIm5vdGVwbGF5ZXIiLCJmaWxlbmFtZSI6ICJub3RlcGxheWVyLmRzcCIsInZlcnNpb24iOiAiMi4yNi4xIiwiY29tcGlsZV9vcHRpb25zIjogIi1sYW5nIHdhc20taWIgLXNjYWwgLWZ0eiAyIiwibGlicmFyeV9saXN0IjogWyIvdXNyL2xvY2FsL3NoYXJlL2ZhdXN0L3N0ZGZhdXN0LmxpYiIsIi91c3IvbG9jYWwvc2hhcmUvZmF1c3Qvb3NjaWxsYXRvcnMubGliIiwiL3Vzci9sb2NhbC9zaGFyZS9mYXVzdC9tYXRocy5saWIiLCIvdXNyL2xvY2FsL3NoYXJlL2ZhdXN0L3BsYXRmb3JtLmxpYiIsIi91c3IvbG9jYWwvc2hhcmUvZmF1c3QvYmFzaWNzLmxpYiJdLCJpbmNsdWRlX3BhdGhuYW1lcyI6IFsiL3Vzci9sb2NhbC9zaGFyZS9mYXVzdCIsIi91c3IvbG9jYWwvc2hhcmUvZmF1c3QiLCIvdXNyL3NoYXJlL2ZhdXN0IiwiLiIsIi9Vc2Vycy9kYW1pZW5jbGFya2UvcHJvamVjdHMvbW9zZmV6LWZhdXN0L25vdGVwbGF5ZXIiXSwic2l6ZSI6IDI2MjQ2NCwiaW5wdXRzIjogMCwib3V0cHV0cyI6IDIsIm1ldGEiOiBbIHsgImJhc2ljcy5saWIvbmFtZSI6ICJGYXVzdCBCYXNpYyBFbGVtZW50IExpYnJhcnkiIH0seyAiYmFzaWNzLmxpYi92ZXJzaW9uIjogIjAuMSIgfSx7ICJmaWxlbmFtZSI6ICJub3RlcGxheWVyLmRzcCIgfSx7ICJtYXRocy5saWIvYXV0aG9yIjogIkdSQU1FIiB9LHsgIm1hdGhzLmxpYi9jb3B5cmlnaHQiOiAiR1JBTUUiIH0seyAibWF0aHMubGliL2xpY2Vuc2UiOiAiTEdQTCB3aXRoIGV4Y2VwdGlvbiIgfSx7ICJtYXRocy5saWIvbmFtZSI6ICJGYXVzdCBNYXRoIExpYnJhcnkiIH0seyAibWF0aHMubGliL3ZlcnNpb24iOiAiMi4zIiB9LHsgIm5hbWUiOiAibm90ZXBsYXllciIgfSx7ICJvc2NpbGxhdG9ycy5saWIvbmFtZSI6ICJGYXVzdCBPc2NpbGxhdG9yIExpYnJhcnkiIH0seyAib3NjaWxsYXRvcnMubGliL3ZlcnNpb24iOiAiMC4xIiB9LHsgInBsYXRmb3JtLmxpYi9uYW1lIjogIkdlbmVyaWMgUGxhdGZvcm0gTGlicmFyeSIgfSx7ICJwbGF0Zm9ybS5saWIvdmVyc2lvbiI6ICIwLjEiIH1dLCJ1aSI6IFsgeyJ0eXBlIjogInZncm91cCIsImxhYmVsIjogIm5vdGVwbGF5ZXIiLCJpdGVtcyI6IFtdfV19"; }
+
+/*
+ faust2wasm: GRAME 2017-2019
+*/
+ 
+'use strict';
+
+// Monophonic Faust DSP
+class noteplayerProcessor extends AudioWorkletProcessor {
+    
+    // JSON parsing functions
+    static parse_ui(ui, obj, callback)
+    {
+        for (var i = 0; i < ui.length; i++) {
+            noteplayerProcessor.parse_group(ui[i], obj, callback);
+        }
+    }
+    
+    static parse_group(group, obj, callback)
+    {
+        if (group.items) {
+            noteplayerProcessor.parse_items(group.items, obj, callback);
+        }
+    }
+    
+    static parse_items(items, obj, callback)
+    {
+        for (var i = 0; i < items.length; i++) {
+            callback(items[i], obj, callback);
+        }
+    }
+    
+    static parse_item1(item, obj, callback)
+    {
+        if (item.type === "vgroup"
+            || item.type === "hgroup"
+            || item.type === "tgroup") {
+            noteplayerProcessor.parse_items(item.items, obj, callback);
+        } else if (item.type === "hbargraph"
+                   || item.type === "vbargraph") {
+            // Nothing
+        } else if (item.type === "vslider"
+                   || item.type === "hslider"
+                   || item.type === "button"
+                   || item.type === "checkbox"
+                   || item.type === "nentry") {
+            obj.push({ name: item.address,
+                     defaultValue: item.init,
+                     minValue: item.min,
+                     maxValue: item.max });
+        }
+    }
+    
+    static parse_item2(item, obj, callback)
+    {
+        if (item.type === "vgroup"
+            || item.type === "hgroup"
+            || item.type === "tgroup") {
+            noteplayerProcessor.parse_items(item.items, obj, callback);
+        } else if (item.type === "hbargraph"
+                   || item.type === "vbargraph") {
+            // Keep bargraph adresses
+            obj.outputs_items.push(item.address);
+            obj.pathTable[item.address] = parseInt(item.index);
+        } else if (item.type === "soundfile") {
+            // Keep soundfile adresses
+            obj.soundfile_items.push(item.address);
+            obj.pathTable[item.address] = parseInt(item.index);
+        } else if (item.type === "vslider"
+                   || item.type === "hslider"
+                   || item.type === "button"
+                   || item.type === "checkbox"
+                   || item.type === "nentry") {
+            // Keep inputs adresses
+            obj.inputs_items.push(item.address);
+            obj.pathTable[item.address] = parseInt(item.index);
+        }
+    }
+ 
+    static get parameterDescriptors() 
+    {
+        // Analyse JSON to generate AudioParam parameters
+        var params = [];
+        noteplayerProcessor.parse_ui(JSON.parse(getJSONnoteplayer()).ui, params, noteplayerProcessor.parse_item1);
+ 	    return params;
+    }
+   
+    constructor(options)
+    {
+        super(options);
+        this.running = true;
+        
+       	const importObject = {
+                env: {
+                    memoryBase: 0,
+                    tableBase: 0,
+
+                    // Integer version
+                    _abs: Math.abs,
+
+                    // Float version
+                    _acosf: Math.acos,
+                    _asinf: Math.asin,
+                    _atanf: Math.atan,
+                    _atan2f: Math.atan2,
+                    _ceilf: Math.ceil,
+                    _cosf: Math.cos,
+                    _expf: Math.exp,
+                    _floorf: Math.floor,
+                    _fmodf: function(x, y) { return x % y; },
+                    _logf: Math.log,
+                    _log10f: Math.log10,
+                    _max_f: Math.max,
+                    _min_f: Math.min,
+                    _remainderf: function(x, y) { return x - Math.round(x/y) * y; },
+                    _powf: Math.pow,
+                    _roundf: Math.fround,
+                    _sinf: Math.sin,
+                    _sqrtf: Math.sqrt,
+                    _tanf: Math.tan,
+                    _acoshf: Math.acosh,
+                    _asinhf: Math.asinh,
+                    _atanhf: Math.atanh,
+                    _coshf: Math.cosh,
+                    _sinhf: Math.sinh,
+                    _tanhf: Math.tanh,
+
+                    // Double version
+                    _acos: Math.acos,
+                    _asin: Math.asin,
+                    _atan: Math.atan,
+                    _atan2: Math.atan2,
+                    _ceil: Math.ceil,
+                    _cos: Math.cos,
+                    _exp: Math.exp,
+                    _floor: Math.floor,
+                    _fmod: function(x, y) { return x % y; },
+                    _log: Math.log,
+                    _log10: Math.log10,
+                    _max_: Math.max,
+                    _min_: Math.min,
+                    _remainder:function(x, y) { return x - Math.round(x/y) * y; },
+                    _pow: Math.pow,
+                    _round: Math.fround,
+                    _sin: Math.sin,
+                    _sqrt: Math.sqrt,
+                    _tan: Math.tan,
+                    _acosh: Math.acosh,
+                    _asinh: Math.asinh,
+                    _atanh: Math.atanh,
+                    _cosh: Math.cosh,
+                    _sinh: Math.sinh,
+                    _tanh: Math.tanh,
+
+                    table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
+                }
+        };
+        
+        this.noteplayer_instance = new WebAssembly.Instance(options.processorOptions.wasm_module, importObject);
+        this.json_object = JSON.parse(options.processorOptions.json);
+     
+        this.output_handler = function(path, value) { this.port.postMessage({ path: path, value: value }); };
+        
+        this.ins = null;
+        this.outs = null;
+
+        this.dspInChannnels = [];
+        this.dspOutChannnels = [];
+
+        this.numIn = parseInt(this.json_object.inputs);
+        this.numOut = parseInt(this.json_object.outputs);
+
+        // Memory allocator
+        this.ptr_size = 4;
+        this.sample_size = 4;
+        this.integer_size = 4;
+        
+        this.factory = this.noteplayer_instance.exports;
+        this.HEAP = this.noteplayer_instance.exports.memory.buffer;
+        this.HEAP32 = new Int32Array(this.HEAP);
+        this.HEAPF32 = new Float32Array(this.HEAP);
+
+        // Warning: keeps a ref on HEAP in Chrome and prevent proper GC
+        //console.log(this.HEAP);
+        //console.log(this.HEAP32);
+        //console.log(this.HEAPF32);
+
+        // bargraph
+        this.outputs_timer = 5;
+        this.outputs_items = [];
+
+        // input items
+        this.inputs_items = [];
+        
+        // soundfile items
+        this.soundfile_items = [];
+
+        // Start of HEAP index
+
+        // DSP is placed first with index 0. Audio buffer start at the end of DSP.
+        this.audio_heap_ptr = parseInt(this.json_object.size);
+
+        // Setup pointers offset
+        this.audio_heap_ptr_inputs = this.audio_heap_ptr;
+        this.audio_heap_ptr_outputs = this.audio_heap_ptr_inputs + (this.numIn * this.ptr_size);
+
+        // Setup buffer offset
+        this.audio_heap_inputs = this.audio_heap_ptr_outputs + (this.numOut * this.ptr_size);
+        this.audio_heap_outputs = this.audio_heap_inputs + (this.numIn * NUM_FRAMES * this.sample_size);
+        
+        // Start of DSP memory : DSP is placed first with index 0
+        this.dsp = 0;
+
+        this.pathTable = [];
+     
+        // Send output values to the AudioNode
+        this.update_outputs = function ()
+        {
+            if (this.outputs_items.length > 0 && this.output_handler && this.outputs_timer-- === 0) {
+                this.outputs_timer = 5;
+                for (var i = 0; i < this.outputs_items.length; i++) {
+                    this.output_handler(this.outputs_items[i], this.HEAPF32[this.pathTable[this.outputs_items[i]] >> 2]);
+                }
+            }
+        }
+        
+        this.initAux = function ()
+        {
+            var i;
+            
+            if (this.numIn > 0) {
+                this.ins = this.audio_heap_ptr_inputs;
+                for (i = 0; i < this.numIn; i++) {
+                    this.HEAP32[(this.ins >> 2) + i] = this.audio_heap_inputs + ((NUM_FRAMES * this.sample_size) * i);
+                }
+                
+                // Prepare Ins buffer tables
+                var dspInChans = this.HEAP32.subarray(this.ins >> 2, (this.ins + this.numIn * this.ptr_size) >> 2);
+                for (i = 0; i < this.numIn; i++) {
+                    this.dspInChannnels[i] = this.HEAPF32.subarray(dspInChans[i] >> 2, (dspInChans[i] + NUM_FRAMES * this.sample_size) >> 2);
+                }
+            }
+            
+            if (this.numOut > 0) {
+                this.outs = this.audio_heap_ptr_outputs;
+                for (i = 0; i < this.numOut; i++) {
+                    this.HEAP32[(this.outs >> 2) + i] = this.audio_heap_outputs + ((NUM_FRAMES * this.sample_size) * i);
+                }
+                
+                // Prepare Out buffer tables
+                var dspOutChans = this.HEAP32.subarray(this.outs >> 2, (this.outs + this.numOut * this.ptr_size) >> 2);
+                for (i = 0; i < this.numOut; i++) {
+                    this.dspOutChannnels[i] = this.HEAPF32.subarray(dspOutChans[i] >> 2, (dspOutChans[i] + NUM_FRAMES * this.sample_size) >> 2);
+                }
+            }
+            
+            // Parse UI
+            noteplayerProcessor.parse_ui(this.json_object.ui, this, noteplayerProcessor.parse_item2);
+                 
+            // Init DSP
+            this.factory.init(this.dsp, sampleRate); // 'sampleRate' is defined in AudioWorkletGlobalScope  
+        }
+
+        this.setParamValue = function (path, val)
+        {
+            this.HEAPF32[this.pathTable[path] >> 2] = val;
+        }
+
+        this.getParamValue = function (path)
+        {
+            return this.HEAPF32[this.pathTable[path] >> 2];
+        }
+
+        // Init resulting DSP
+        this.initAux();
+        console.log(this);
+    }
+    
+    handleMessage(event)
+    {
+        var msg = event.data;
+        switch (msg.type) {
+            case "destroy": this.running = false; break;
+        }
+    }
+    
+    process(inputs, outputs, parameters) 
+    {
+        var input = inputs[0];
+        var output = outputs[0];
+        
+        // Check inputs
+        if (this.numIn > 0 && (!input || !input[0] || input[0].length === 0)) {
+            //console.log("Process input error");
+            return true;
+        }
+        // Check outputs
+        if (this.numOut > 0 && (!output || !output[0] || output[0].length === 0)) {
+            //console.log("Process output error");
+            return true;
+        }
+        
+        // Copy inputs
+        if (input !== undefined) {
+            for (var chan = 0; chan < Math.min(this.numIn, input.length); ++chan) {
+                var dspInput = this.dspInChannnels[chan];
+                dspInput.set(input[chan]);
+            }
+        }
+        
+        /*
+        TODO: sample accurate control change is not yet handled
+        When no automation occurs, params[i][1] has a length of 1,
+        otherwise params[i][1] has a length of NUM_FRAMES with possible control change each sample
+    	*/
+        
+        // Update controls
+        for (const path in parameters) {
+            const paramArray = parameters[path];
+            this.setParamValue(path, paramArray[0]);
+        }
+        
+        // Compute
+        try {
+            this.factory.compute(this.dsp, NUM_FRAMES, this.ins, this.outs);
+        } catch(e) {
+            console.log("ERROR in compute (" + e + ")");
+        }
+        
+        // Update bargraph
+        this.update_outputs();
+        
+        // Copy outputs
+        if (output !== undefined) {
+            for (var chan = 0; chan < Math.min(this.numOut, output.length); ++chan) {
+                var dspOutput = this.dspOutChannnels[chan];
+                output[chan].set(dspOutput);
+            }
+        }
+        
+        return this.running;
+    }
+}
+
+// Globals
+const NUM_FRAMES = 128;
+try {
+    registerProcessor('noteplayer', noteplayerProcessor);
+} catch (error) {
+    console.warn(error);
+}
